34d3df3e31e645295063fae6801ff9e6f4e89b0c
tsubasa <tsubasa@tsubasanoMacBook-Pro.local>
2022-09-02 21:01:32 +0900

    keijiban

---
+++ 4each_keijiban/insert.php
@@ -0,0 +1,21 @@
+<?php
+
+mb_internal_encoding("utf8");
+
+require "DB.php";
+$db = new DB();
+$pdo = $db->connect();
+
+$stmt = $pdo -> prepare($db->insert());
+
+$stmt->bindValue(1,$_POST['handlename']);
+$stmt->bindValue(2,$_POST['title']);
+$stmt->bindValue(3,$_POST['comments']);
+
+$stmt->execute();
+$pdo = NULL;
+
+
+header("Location:http://localhost/4each_keijiban/index.php");
+
+?>
